11e0fd27929795427bd9c5f02614a1cd918299b9
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-08-26 22:17:11 +0300

    fix markup and styles

--- src/js/oneCardMarkup.js
+++ src/js/oneCardMarkup.js
@@ -1,17 +1,27 @@
 function oneCardMarkup(item) {
-    return `
-    <div class="photo-card">
-        <a class="photo-card__link">
-            <img class="photo-card-img" src="https://image.tmdb.org/t/p/w500${item.poster_path}" alt="https://image.tmdb.org/t/p/w500${item.backdrop_path}" />
-        </a>
-    <div class="card-info">
-        <a>
-            <p class="card-info__title">${item.title}</p>
-            <p class="card-info__genre">
-            ${item.genre_ids}<span class="info-separator">${item.release_date}</span><span class="film-raiting">${item.vote_average}</span>
-            </p>
-        </a>
-    </div>
-</div>`;
+  return `
+        <div class="photo-card" data-id="${item.id}">
+      <a class="photo-card__link">
+        <img
+          class="photo-card-img"
+          src="https://image.tmdb.org/t/p/w500${item.poster_path}"
+          alt="https://image.tmdb.org/t/p/w500${item.backdrop_path}"
+        />
+      </a>
+      <div class="card-info">
+        <h3 class="card-info__title">${item.title}</h3>
+        <p>
+          <span class="card-info__genre">
+          ${item.genre_ids}
+          </span>
+          <span class="release-date">
+          ${Number.parseInt(item.release_date)}
+          </span>
+          <span class="film-raiting">
+          ${item.vote_average}
+          </span>
+        </p>
+      </div>
+    </div>`;
 }
 export { oneCardMarkup };
