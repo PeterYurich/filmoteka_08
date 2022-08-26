0965138740b72f1fe27929c8574917c723804018
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-08-26 23:51:43 +0300

    add toFixed(1) in html and remove position in rating

--- src/js/oneCardMarkup.js
+++ src/js/oneCardMarkup.js
@@ -1,26 +1,27 @@
 function oneCardMarkup(item) {
-    return `
+  return `
     <div class="photo-card" data-id="${item.id}">
-    <a class="photo-card__link">
-        <img class="photo-card-img" src="https://image.tmdb.org/t/p/w500${item.poster_path}"
-        alt="https://image.tmdb.org/t/p/w500${item.backdrop_path}" />
-    </a>
-    <div class="card-info">
+      <a class="photo-card__link">
+        <img
+          class="photo-card-img"
+          src="https://image.tmdb.org/t/p/w500${item.poster_path}"
+          alt="https://image.tmdb.org/t/p/w500${item.backdrop_path}"
+        />
+      </a>
+      <div class="card-info">
         <h3 class="card-info__title">${item.title}</h3>
         <p>
-        <span class="card-info__genre">
-            ${item.genre_ids}
-        </span>
-        <span class="release-date">
-            ${Number.parseInt(item.release_date)}
-        </span>
-        <span class="film-rating">
-            ${item.vote_average}
-        </span>
+          <span class="card-info__genre">
+          ${item.genre_ids}
+          </span>
+          <span class="release-date">
+          ${Number.parseInt(item.release_date)}
+          </span>
+          <span class="film-rating">
+          ${item.vote_average.toFixed(1)}
+          </span>
         </p>
-    </div>
-    </div>
-`;
-
+      </div>
+    </div>`;
 }
 export { oneCardMarkup };
