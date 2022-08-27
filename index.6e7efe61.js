5c01f7ca9a52770ca296c8bc05357b49e99d3dbc
PeterYurich <97597653+PeterYurich@users.noreply.github.com>
2022-08-27 22:14:29 +0000

    Deploying to gh-pages from @ PeterYurich/filmoteka_08@dec922ef3f27c277cdc5bb94780e0e945cfb533e 🚀

--- index.6e7efe61.js
+++
@@ -1,2 +0,0 @@
-var refsBtn={addToWatched:document.querySelector(".btn-add-to-watched"),addToQueue:document.querySelector(".btn-add-to-queue")};function onAddToWatched(){refsBtn.addToWatched.textContent="Remove watched"}function onAddToQueue(){refsBtn.addToQueue.textContent="Remove queue"}refsBtn.addToWatched.addEventListener("click",onAddToWatched),refsBtn.addToQueue.addEventListener("click",onAddToQueue);
-//# sourceMappingURL=index.6e7efe61.js.map
