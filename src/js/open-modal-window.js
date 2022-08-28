ccb079ebe6558f928c6380c8cc3a98b34b2cb33f
Alona Beskhlibnova <a.beskhlibnova@gmail.com>
2022-08-29 01:44:28 +0300

    fixed add to LS

--- src/js/open-modal-window.js
+++ src/js/open-modal-window.js
@@ -4,19 +4,22 @@ const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
+  body: document.querySelector('body'),
 };
 
 refs.modal.addEventListener('click', onBackdropClick);
 window.addEventListener('keydown', onEscClose);
 
-export function onOpenModal(e) {
+export function onOpenModal() {
   window.addEventListener('keydown', onEscClose);
   refs.modal.classList.remove('is-hidden');
+  refs.body.classList.add('overflow-hidden');
 }
 
 export function onCloseModal() {
   window.removeEventListener('keydown', onEscClose);
   refs.modal.classList.add('is-hidden');
+  refs.body.classList.remove('overflow-hidden');
 }
 
 function onBackdropClick(e) {
