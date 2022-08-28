023cc4e28aecb5fb81c8771c168e2459bf31fe4a
Yevhen Prokhorenko <prokhorenko.yevhen@gmail.com>
2022-08-29 01:54:20 +0300

    add pagination to  load-pop-movies

--- src/js/load-pop-movies.js
+++ src/js/load-pop-movies.js
@@ -1,30 +1,33 @@
 import { oneCardMarkup } from './oneCardMarkup';
-import { TheMovieDb } from "./fetch";
-import { getTheMoviesTargetInfo } from "./get-target-movies"
+import { TheMovieDb } from './fetch';
+import { getTheMoviesTargetInfo } from './get-target-movies';
+import { makePaginationMarkup } from './custom-paginat';
 
-const mediaType = "movie"
-const timeWindow = "day"
+const mediaType = 'movie';
+const timeWindow = 'day';
 
 const fetchMovies = new TheMovieDb();
 
 async function loadRequestedMovies(mediaType, timeWindow) {
+  try {
+    const ApiReply = await fetchMovies.fetchPopularMovies(
+      mediaType,
+      timeWindow
+    );
 
-    try {
-        const ApiReply = await fetchMovies.fetchPopularMovies(mediaType, timeWindow);
+    const foundMovies = ApiReply.results;
+    const foundMoviesIds = foundMovies.map(film => film.id);
+    const FilmsToRender = await getTheMoviesTargetInfo(foundMoviesIds);
 
-        const foundMovies = ApiReply.results
-        const foundMoviesIds = foundMovies.map(film => film.id)
-        const FilmsToRender = await getTheMoviesTargetInfo(foundMoviesIds)
+    const markup = await FilmsToRender.map(film => {
+      return oneCardMarkup(film);
+    }).join('');
 
-        const markup = await FilmsToRender.map(film => {
-            return oneCardMarkup(film)
-        }).join('');
-
-        const containerMainPage = document.querySelector('.film-grid');
-        containerMainPage.innerHTML = markup
-    } catch (error) {
-        console.log(error)
-    }
+    const containerMainPage = document.querySelector('.film-grid');
+    containerMainPage.innerHTML = markup;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-loadRequestedMovies(mediaType, timeWindow)
+loadRequestedMovies(mediaType, timeWindow);
