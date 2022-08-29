a5f8031e22f480988bdd6fba79e1028c9a36a71d
Peter <peteryurich@gmail.com>
2022-08-29 07:50:20 +0300

    ready pagination

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import './js/loader';
 import './js/load-pop-movies';
 import './js/load-requested-movies';
-import './js/custom-pagination';
 
 import './js/open-modal-window';
 import './js/modal-footer-window';
 import './js/load-clicked-movie';
+
+class Asd {
+    constructor({a}) {
+        this.a = a;
+        this.b = "lkn";
+        this.c = 3;
+    }
+} 
+
+const df = 5
+
+const qwe = new Asd({a: df})
