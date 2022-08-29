079771ef07ca2dcedc2a12595039925268f8d452
Люда <1982luda30@gmail.com>
2022-08-29 11:16:16 +0300

    fixed footer

--- src/js/modal-footer-window.js
+++ src/js/modal-footer-window.js
@@ -1,9 +1,10 @@
-
 const refsTeam = {
-    openModalBtnTeam: document.querySelector('[data-modal-open-team]'),
-    closeModalBtnTeam: document.querySelector('[data-modal-close-team]'),
-    modalTeam: document.querySelector('[data-modal-team]'),
+  openModalBtnTeam: document.querySelector('[data-modal-open-team]'),
+  closeModalBtnTeam: document.querySelector('[data-modal-close-team]'),
+  modalTeam: document.querySelector('[data-modal-team]'),
+  body: document.querySelector('body'),
 };
+
 console.log(refsTeam.openModalBtnTeam);
 refsTeam.openModalBtnTeam.addEventListener('click', onOpenModalTeam);
 refsTeam.closeModalBtnTeam.addEventListener('click', onCloseModalTeam);
@@ -11,23 +12,57 @@ refsTeam.modalTeam.addEventListener('click', footerModalClickTeam);
 window.addEventListener('keydown', onEscCloseTeam);
 
 function onOpenModalTeam(e) {
-    e.preventDefault();
-    window.addEventListener('keydown', onEscCloseTeam);
-    refsTeam.modalTeam.classList.remove('is-hidden');
+  e.preventDefault();
+  window.addEventListener('keydown', onEscCloseTeam);
+  refsTeam.modalTeam.classList.remove('is-hidden');
+  refsTeam.body.classList.add('body-hiden');
 }
 
 function onCloseModalTeam() {
-    window.removeEventListener('keydown', onEscCloseTeam);
-    refsTeam.modalTeam.classList.add('is-hidden');
+  window.removeEventListener('keydown', onEscCloseTeam);
+  refsTeam.modalTeam.classList.add('is-hidden');
+  refsTeam.body.classList.remove('body-hiden');
 }
 
 function footerModalClickTeam(e) {
-    if (e.currentTarget === e.target) {
-        onCloseModalTeam();
-    }
+  if (e.currentTarget === e.target) {
+    onCloseModalTeam();
+  }
 }
 function onEscCloseTeam(e) {
-    if (e.key === 'Escape') {
-        onCloseModalTeam();
-    }
+  if (e.key === 'Escape') {
+    onCloseModalTeam();
+  }
 }
+// const refsTeam = {
+//     openModalBtnTeam: document.querySelector('[data-modal-open-team]'),
+//     closeModalBtnTeam: document.querySelector('[data-modal-close-team]'),
+//     modalTeam: document.querySelector('[data-modal-team]'),
+// };
+// console.log(refsTeam.openModalBtnTeam);
+// refsTeam.openModalBtnTeam.addEventListener('click', onOpenModalTeam);
+// refsTeam.closeModalBtnTeam.addEventListener('click', onCloseModalTeam);
+// refsTeam.modalTeam.addEventListener('click', footerModalClickTeam);
+// window.addEventListener('keydown', onEscCloseTeam);
+
+// function onOpenModalTeam(e) {
+//     e.preventDefault();
+//     window.addEventListener('keydown', onEscCloseTeam);
+//     refsTeam.modalTeam.classList.remove('is-hidden');
+// }
+
+// function onCloseModalTeam() {
+//     window.removeEventListener('keydown', onEscCloseTeam);
+//     refsTeam.modalTeam.classList.add('is-hidden');
+// }
+
+// function footerModalClickTeam(e) {
+//     if (e.currentTarget === e.target) {
+//         onCloseModalTeam();
+//     }
+// }
+// function onEscCloseTeam(e) {
+//     if (e.key === 'Escape') {
+//         onCloseModalTeam();
+//     }
+// }
