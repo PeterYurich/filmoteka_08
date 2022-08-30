0af1577f035d05ed874db1539d75cb2d59ff375b
Люда <1982luda30@gmail.com>
2022-08-30 10:42:33 +0300

    fixed size modal foto

--- src/js/modal-footer-window.js
+++ src/js/modal-footer-window.js
@@ -23,45 +23,13 @@ function onCloseModalTeam() {
   refsTeam.body.classList.remove('body-hiden');
 }
 
-function footerModalClickTeam(e) {
-  if (e.currentTarget === e.target) {
-    onCloseModalTeam();
-  }
-}
+// function footerModalClickTeam(e) {
+//   if (e.currentTarget === e.target) {
+//     onCloseModalTeam();
+//   }
+// }
 function onEscCloseTeam(e) {
   if (e.key === 'Escape') {
     onCloseModalTeam();
   }
 }
-// const refsTeam = {
-//     openModalBtnTeam: document.querySelector('[data-modal-open-team]'),
-//     closeModalBtnTeam: document.querySelector('[data-modal-close-team]'),
-//     modalTeam: document.querySelector('[data-modal-team]'),
-// };
-// console.log(refsTeam.openModalBtnTeam);
-// refsTeam.openModalBtnTeam.addEventListener('click', onOpenModalTeam);
-// refsTeam.closeModalBtnTeam.addEventListener('click', onCloseModalTeam);
-// refsTeam.modalTeam.addEventListener('click', footerModalClickTeam);
-// window.addEventListener('keydown', onEscCloseTeam);
-
-// function onOpenModalTeam(e) {
-//     e.preventDefault();
-//     window.addEventListener('keydown', onEscCloseTeam);
-//     refsTeam.modalTeam.classList.remove('is-hidden');
-// }
-
-// function onCloseModalTeam() {
-//     window.removeEventListener('keydown', onEscCloseTeam);
-//     refsTeam.modalTeam.classList.add('is-hidden');
-// }
-
-// function footerModalClickTeam(e) {
-//     if (e.currentTarget === e.target) {
-//         onCloseModalTeam();
-//     }
-// }
-// function onEscCloseTeam(e) {
-//     if (e.key === 'Escape') {
-//         onCloseModalTeam();
-//     }
-// }
