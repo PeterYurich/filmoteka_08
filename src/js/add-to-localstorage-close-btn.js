0df62191b8ba5e7c10f4b9962dce022252b607e1
Alona Beskhlibnova <a.beskhlibnova@gmail.com>
2022-08-30 22:08:29 +0300

    fixed change with wathed / queue

--- src/js/add-to-localstorage-close-btn.js
+++ src/js/add-to-localstorage-close-btn.js
@@ -31,11 +31,18 @@ export function addToWatchedAddToQueueCloseModal(id) {
       addToWatched.classList.remove('btn-remove');
       addToWatched.textContent = 'Add to watched';
     } else {
+      if (positionArrayQueue >= 0) {
+        myLibrary.queue.splice(positionArrayQueue, 1);
+        addToQueue.classList.remove('btn-remove');
+        addToQueue.textContent = 'Add to queue';
+        localStorage.setItem('queue', JSON.stringify(myLibrary.queue));
+      }
       myLibrary.watched.push(idMovie);
-      myLibrary.queue.splice(positionArrayQueue, 1);
+
       addToWatched.classList.add('btn-remove');
       addToWatched.textContent = 'Remove watched';
     }
+
     localStorage.setItem('watched', JSON.stringify(myLibrary.watched));
   }
   addToWatched.addEventListener('click', onAddToWatched);
@@ -51,12 +58,19 @@ export function addToWatchedAddToQueueCloseModal(id) {
       addToQueue.classList.remove('btn-remove');
       addToQueue.textContent = 'Add to queue';
     } else {
+      if (positionArrayWatched >= 0) {
+        myLibrary.watched.splice(positionArrayWatched, 1);
+        addToWatched.classList.remove('btn-remove');
+        addToWatched.textContent = 'Add to watched';
+        localStorage.setItem('watched', JSON.stringify(myLibrary.watched));
+      }
       myLibrary.queue.push(idMovie);
-      myLibrary.watched.splice(positionArrayWatched, 1);
+
       addToQueue.classList.add('btn-remove');
       addToQueue.textContent = 'Remove queue';
     }
     localStorage.setItem('queue', JSON.stringify(myLibrary.queue));
+    localStorage.setItem('watched', JSON.stringify(myLibrary.watched));
   }
   addToQueue.addEventListener('click', onAddToQueue);
 
