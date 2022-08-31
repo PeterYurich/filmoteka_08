6d989e75c3646af05108b8e4a41ec335cc52ac1d
Peter <peteryurich@gmail.com>
2022-08-31 15:55:41 +0300

    fixed export 2

--- src/js/get-content/load-pop-clicked-page.js
+++ src/js/get-content/load-pop-clicked-page.js
@@ -1,38 +1,38 @@
 import { TheMovieDb } from "./fetch.js"
 import { oneCardMarkup } from '../components/one-card-markup';
 import { getTheMoviesTargetInfo } from './get-movies-target-info';
-import { setPopPagination, paginationWrapper } from '../custom-pagination/set-pop-pagination';
-
+import { setPopPagination } from '../custom-pagination/set-pop-pagination';
+import { paginationWrapper } from '../custom-pagination/make-pagination-markup'
 async function loadPopClickedPage(e) {
-    e.preventDefault();
-    if (e.target.nodeName !== "BUTTON") { return }
+        e.preventDefault();
+        if (e.target.nodeName !== "BUTTON") { return }
 
 
-    paginationWrapper.innerHTML = '';
+        paginationWrapper.innerHTML = '';
 
-    const clickedPage = e.target.textContent;
-    const fetchMovies = new TheMovieDb(clickedPage)
-    // fetchMovies.page(clickedPage)
+        const clickedPage = e.target.textContent;
+        const fetchMovies = new TheMovieDb(clickedPage)
+        // fetchMovies.page(clickedPage)
 
-    try {
-        const apiReply = await fetchMovies.fetchPopularMovies();
+        try {
+            const apiReply = await fetchMovies.fetchPopularMovies();
 
-        const foundMovies = apiReply.results;
-        const foundMoviesIds = foundMovies.map(film => film.id);
-        const filmsToRender = await getTheMoviesTargetInfo(foundMoviesIds);
+            const foundMovies = apiReply.results;
+            const foundMoviesIds = foundMovies.map(film => film.id);
+            const filmsToRender = await getTheMoviesTargetInfo(foundMoviesIds);
 
-        const markup = filmsToRender.map(film => {
-            return oneCardMarkup(film);
-        }).join('');
+            const markup = filmsToRender.map(film => {
+                return oneCardMarkup(film);
+            }).join('');
 
-        const containerMainPage = document.querySelector('.film-grid');
-        containerMainPage.innerHTML = markup;
+            const containerMainPage = document.querySelector('.film-grid');
+            containerMainPage.innerHTML = markup;
 
-        setPopPagination(apiReply);
+            setPopPagination(apiReply);
 
-    } catch (error) {
-        console.log(error);
+        } catch (error) {
+            console.log(error);
+        }
     }
-}
 
 export { loadPopClickedPage }
